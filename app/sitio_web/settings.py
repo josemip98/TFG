0e0e1a580ddab8fec132680034a2eb64a4c7e77d
Jose Miguel Pelegrina <jmpelegrina@dnota.com>
2021-08-11 20:32:49 +0200

    Cambios en settings para acceso a BD. #23

--- app/sitio_web/settings.py
+++ app/sitio_web/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,6 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 AUTH_USER_MODEL = 'aplicacion.usuario'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
