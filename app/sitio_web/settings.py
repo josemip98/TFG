f0fd0ebe981b0780ead158dce3d9632d2846a416
Jose Miguel Pelegrina <37540705+josemip98@users.noreply.github.com>
2021-08-12 12:19:41 +0200

    Update settings.py

--- app/sitio_web/settings.py
+++ app/sitio_web/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.dirname(os.path.dirname(__file__))
 MEDIA_URL = '/media/'
