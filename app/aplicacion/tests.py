82ba03a2dfe3db12718493f369ead77e27561ce4
Jose Miguel Pelegrina <j_miguel98@hotmail.com>
2021-08-29 20:51:24 +0200

    Creados tests para crear y modificar producto. close #7, close #8

--- app/aplicacion/tests.py
+++ app/aplicacion/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Producto
+
+# Tests clase producto
+class ProductoModelTest(TestCase):
+
+    @classmethod
+    def testCrearProducto(self):
+        print("Test crear producto")
+        self.test_producto = Producto(nombre="Arroz")
+        self.test_producto.save()
+
+    def testModificarProducto(self):
+        print("Test modificar producto")
+        self.test_producto = Producto(nombre="Arroz", calorias=10, grasa=10, proteinas=10)
+        self.test_producto.save()
+        Producto.objects.filter(nombre="Arroz").update(calorias=20)
